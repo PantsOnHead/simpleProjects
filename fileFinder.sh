5bfc46644e7cf1617773ed903ee5f4db1adcd8eb
pugilist <myevilempire@gmail.com>
2015-12-13 18:19:47 -0500

    remove os_x function and wrote DATE_TIME_SEARCH function to replace both LINUX and OS_X search subroutines

--- fileFinder.sh
+++ fileFinder.sh
@@ -8,108 +8,56 @@ function PRESS_ENTER
     clear
 }
 
-function OS_X {
-    echo "**********************"
-    echo "** File Search OS X **"
-    echo "**********************"
-    # Ask for user input on where to start the search
+function DATE_TIME_SEARCH {
+    echo "*************************"
+    echo "** File search by date **"
+    echo "*************************"
+
     echo -n "Enter a folder to scan: "
     read folder
-    # Check to see if the folder is a real directory the program can see loop until input is valid
-    while [ ! -d "$folder" ]; do
+    while [ ! -d "${folder}" ]; do
         echo "ERROR. Invalid directory."
         echo ""
         echo -n "Enter a folder to scan: "
         read folder
     done
 
-    # The idea here is to look for files modified within a specific time period. 
     echo -n "Enter start date (Format YYYY-MM-DD): "
     read startDate
-    # This is rough validaion, as it wont actually check to see if the date is valid, just the format.
-    # It also validates the optional time setting to the correct format.
-    while [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
-        echo "ERROR. Invalid date format."
-        echo ""
+    while [[ ! "${startDate}" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! "${startDate}" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
+        echo -e "ERROR. Invalid date format.\n"
         echo -n "Enter start date (Format YYYY-MM-DD): "
         read startDate
     done
-    
-    echo -n "Enter end date (`date +%Y-%m-%d`): "
+
+    echo -n "Enter end date (Format YYYY-MM-DD) [Default: $(date +%Y-%m-%d)]: "
     read endDate
+
+    while [ "${endDate}" != "" ] && [[ ! "${startDate}" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! "${startDate}" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
+        echo -e "ERROR. Invalid date format.\n"
+        echo -n "Enter end date (Format YYYY-MM-DD) [Default: $(date +%Y-%m-%d)]: "
+        read endDate
+    done
     # If no input then grab current system time. 
-    if ["$endDate" = ""]; then
+    if [ "${endDate}" = "" ]; then
         endDate=`date +%Y-%m-%d`
-    fi        
-
-    while [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
-        echo "ERROR. Invalid date format."
-        echo ""
-        echo -n "Enter end date (Format YYYY-MM-DD): "
-        read startDate
-    done
-
-    #gfind $folder -type f -newermt $startDate ! -newermt $endDate -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' '
-
-    #find . -type f -newermt 2015-10-01 ! -newermt 2015-12-09 -exec stat -f "%Sp %Su %Sg %z %t%Sm %N %t MD5: " {} \; md5 {} \; | cut -f 2- -d '='
-
-    # Run find with the users input. While loop reads in the returned files while handling if we run into spaces in the file name 
-    find $folder -type f -newermt $startDate ! -newermt $endDate -print0 | while read -d $'\0' file; do
-        #echo $file
-        # Run stat on each file returned then format the results.
-        # stat format translaton: Permissions, User ID, Group ID, Size (bytes), Time Modified, Name
-        stat -f "%Sp %Su %Sg %z %t%Sm %N" "$file"
-        # Calculate MD5 hash of retuned files.
-        echo "MD5: $(md5 "$file"| cut -f 2- -d '=')"
-        echo ""
-    done
-}
-
-
-
-
-
-function LINUX {
-    echo "***********************"
-    echo "** File Search Linux **"
-    echo "***********************"
-
-    echo -n "Enter a folder to scan: "
-    read folder
-    while [ ! -d "$folder" ]; do
-        echo "ERROR. Invalid directory."
-        echo ""
-        echo -n "Enter a folder to scan: "
-        read folder
-    done
-
-    echo -n "Enter start date (Format YYYY-MM-DD): "
-    read startDate
-    while [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
-        echo "ERROR. Invalid date format."
-        echo ""
-        echo -n "Enter start date (Format YYYY-MM-DD): "
-        read startDate
-    done
-
-    echo -n "Enter end date (Format YYYY-MM-DD): "
-    read endDate
-    while [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]] && [[ ! $startDate =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}.[0-9]{2}:[0-9]{2}:[0-9]{2}$ ]]; do
-        echo "ERROR. Invalid date format."
-        echo ""
-        echo -n "Enter end date (Format YYYY-MM-DD): "
-        read startDate
-    done
+    fi       
 
     while read -r file; do
         #echo $file
         # Run stat on each file returned then format the results.
         # stat format translaton: Permissions, User ID, Group ID, Size (bytes), Time Modified, Name
-        stat -c "%A %U %G %s %y %n" "$file"
-        # Calculate MD5 hash of retuned files.
-        echo "MD5: $(md5sum "$file"| cut -f 2- -d ' ')"
-        echo ""
-    done < <(find $folder -type f -newermt $startDate ! -newermt $endDate)
+        # then calculate md5 hash for each file returned
+
+        if [ ${is_GNU} ]
+        then
+            stat -c "%A %U %G %s %y %n" "${file}"
+            echo -e "MD5: $(md5sum "${file}"| cut -f 2- -d ' ')\n"
+        else
+            stat -f "%Sp %Su %Sg %z %t%Sm %N" "$file"
+            echo -e "MD5: $(md5 "$file"| cut -f 2- -d '=')\n"
+        fi
+    done < <(find ${folder} -type f -newermt $startDate ! -newermt $endDate)
 
 }
 
@@ -182,9 +130,8 @@ until [ "$selection" = "0" ]; do
 	echo ""
     echo ""
     echo "MAIN MENU"
-    echo "1 - Search on OS X"
-    echo "2 - Search on Linux"
-    echo "3 - Search for sha1 hash"
+    echo "1 - Search by Date / Time"
+    echo "2 - Search by sha1 hash"
     echo ""
     echo "0 - exit program"
     echo ""
@@ -192,9 +139,8 @@ until [ "$selection" = "0" ]; do
     read selection
     echo ""
     case $selection in
-    	1 ) clear; OS_X ; PRESS_ENTER ;;
-        2 ) clear; LINUX ; PRESS_ENTER ;;
-        3 ) clear; SHA1_SEARCH ; PRESS_ENTER ;;
+    	1 ) clear; DATE_TIME_SEARCH ; PRESS_ENTER ;;
+        2 ) clear; SHA1_SEARCH ; PRESS_ENTER ;;
         0 ) exit ;;
         * ) echo "Please enter 1, 2, or 0"; PRESS_ENTER
     esac
